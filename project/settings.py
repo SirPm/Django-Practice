db726432f8a9faa3f04fc9ac9d8e789a73a130eb
SirPm <odumosuoluwapm@gmail.com>
2020-10-28 19:49:16 +0100

    dj_database

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 # python manage.py runserver
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
+import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
 
